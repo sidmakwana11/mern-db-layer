f85cd417bf7bae8dc750b23dd7a4ceffe15c586a
Siddharth Makwana <112619536+sidmakwana11@users.noreply.github.com>
2025-04-11 13:04:52 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -2,7 +2,11 @@ const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
   username: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
+  email: { type: String,
+  required: true,
+  unique: true,
+  lowercase: true,
+  trim: true},
   password: { type: String, required: true },
 });
 
