a71df8dfe82459471bebf4cf140f3e53f485f13f
Siddharth Makwana <112619536+sidmakwana11@users.noreply.github.com>
2025-04-21 13:07:58 +0530

    Update Todo.js

--- models/Todo.js
+++ models/Todo.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const TodoSchema = new mongoose.Schema({
   title: { type: String, required: true },
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  image: { data: Buffer,type: String } 
 });
 
 module.exports = mongoose.model('Todo', TodoSchema);
